762f79ea6b5be61a0e58d836acb067ceb5c92800
Muhammad Yahya <moch.yahya95@gmail.com>
2021-02-26 21:20:01 +0700

    first upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1614348857,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/d101dc0b5787325e7e7465975fbfc6c-sheet0.png",
+		"images/d101dc0b5787325e7e7465975fbfc6c2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
